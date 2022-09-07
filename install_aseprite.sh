b8839b8cd4c2c52c3f3d151e9c3a3bbf0b9108c9
D4ykoo <50523358+D4ykoo@users.noreply.github.com>
2022-09-07 07:27:43 +0200

    Fix: Some little path mistakes

--- install_aseprite.sh
+++ install_aseprite.sh
@@ -2,24 +2,23 @@ echo "Creating installation folder..."
 mkdir AsepriteInstallation
 cd AsepriteInstallation
 echo "Done!"
-
+pwd
 echo "Copying and extracting the debian file..."
-mv /path/to/Aseprite_1.X-xxxx_amd64.deb                				# replace here with your path to the aseprite file
-ar x /path/to/Aseprite_1.X-xxxx_amd64.deb					# same here	
-tar xvf control.tar.gz
-tar xvf data.tar.gz
-echo "Done!"
+mv ../Aseprite_1.3-beta21-1_amd64.deb .                                         # replace here with your path to the aseprite file
+echo "Start extracting...!"
+ar x Aseprite_1.3-beta21-1_amd64.deb                                            # same here
+tar xvf control.tar.xz
+tar xvf data.tar.xz
+echo "Done extracting!"
 
 # todo: verify checksums
 
 echo "Start copying the binary and additional files..."
-sudo cp ./usr/bin/aseprite /usr/bin/
-sudo cp ./usr/share/applications
-sudo cp applications/aseprite.desktop /usr/share/applications
-sudo cp -rf aseprite/ /usr/share/
-sudo cp -rf doc/ /usr/share/doc/
+sudo cp -rf ./usr/bin/ /usr/bin/
+sudo cp -rf ./usr/share/applications /usr/share/applications
+sudo cp -rf ./usr/share/aseprite/ /usr/share/
+sudo cp -rf ./usr/share/doc/ /usr/share/doc/
 echo "Done!"
 echo "Installation completed!"
 
 # todo: make icon working
-
